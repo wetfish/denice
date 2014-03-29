845b6c3774084847e22c7b262da57f8f6bd4738e
lq <lq@le1.ca>
2014-03-30 02:11:48 +0400

    blaaah

--- lua/main.lua
+++ lua/main.lua
@@ -10,12 +10,3 @@ dofile("lua/join.lua")
 dofile("lua/talk.lua")
 dofile("lua/quotes.lua")
 dofile("lua/last.lua")
-
-function debug_callback(event, origin, params)
-	print(event.." ("..origin..")")
-	for i,v in pairs(params) do
-		print(" "..i..": "..v)
-	end
-end
-register_callback("NOTICE", "debug_callback")
-register_callback("433", "debug_callback")
