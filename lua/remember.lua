219a5dd9f0a314c58176bebaaf91283fa9d8eaaf
lq <lq@le1.ca>
2014-03-30 01:00:09 -0500

    i wish i didnt have to push/pull every time i wanted to test my code

--- lua/remember.lua
+++ lua/remember.lua
@@ -22,6 +22,9 @@ function remember(s,w,t)
 	if s == nil or not validReq(s) then
 		print("printing list of things i remember")
 		local cur = sql_query("SELECT DISTINCT `Title` FROM `remember` ORDER BY `Title` ASC")
+		if sql_errno() != 0 then
+			print(sql_error())
+		end
 		print("cur="..cur)
 		local outstr = "I know about: "
 		while true do
