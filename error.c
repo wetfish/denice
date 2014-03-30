8fb7248a726fac9095baea083155d4cd94c82ab2
lq <lq@le1.ca>
2014-03-29 19:31:37 -0500

    fixed the segfault when handling errors

--- error.c
+++ error.c
@@ -7,24 +7,27 @@
 
 // Print formatted error and if error is fatal, terminate
 void error(int fatal, char* format, ...){
+	char str[256];
 	va_list args;
 	va_start(args, format);
-	vfprintf(stderr, format, args);
+	// put the error msg in local array
+	vsnprintf(str, 256, format, args);
+	va_end(args);
+	// print it out
+	fprintf(stderr, "%s", str);
+	
 	if(fatal){
-		va_end(args);
 		fprintf(stderr,"Fatal error... Exiting.\n");
 		exit(1);
 	}
 	else if(I){
-		char str[256];
+		// generate ERROR event to handle in scripts
 		const char** strp = malloc(sizeof(char*));
 		strp[0] = str;
 		fprintf(stderr,"Generating ERROR event...\n");
-		vsnprintf(str, 256, format, args);
-		va_end(args);
 		if(str[strlen(str)-1] == '\n')
 			str[strlen(str)-1] = '\0';
-		event_generic(I, "ERROR", "<ERROR>", strp, 1);
+		event_generic(I, "ERROR", "(core)", strp, 1);
 		free(strp);
 	}
 	
