80629f732e8b358f2b518fbedbb9dbe6034e0ca3
lq <lq@le1.ca>
2014-03-31 20:31:45 -0600

    trim trailing whitespace off dickbutt

--- lua/dickbutt.lua
+++ lua/dickbutt.lua
@@ -2,7 +2,7 @@ function dickbutt_callback(event, origin, params)
 	local msg_parts = str_split_max(params[2], " ",2)
 	if msg_parts[1] == "!dickbutt" then
 		for line in io.lines("lua/dickbutt.txt") do
-			irc_msg(params[1], line)
+			irc_msg(params[1], line:gsub(" +$",""))
 		end
 	end
 end
