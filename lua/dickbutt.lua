86d3eaee0473ed46d7b4814739448d2e6374ff8b
lq <lq@le1.ca>
2014-03-31 20:43:41 -0600

    add timer to dickbutt

--- lua/dickbutt.lua
+++ lua/dickbutt.lua
@@ -1,8 +1,15 @@
+dickbutt_table = {}
+
 function dickbutt_callback(event, origin, params)
 	local msg_parts = str_split_max(params[2], " ",2)
 	if msg_parts[1] == "!dickbutt" then
-		for line in io.lines("lua/dickbutt.txt") do
-			irc_msg(params[1], line:gsub(" +$",""))
+		if dickbutt_table[origin] == nil or dickbutt_table[origin] < os.time() - 60 then
+			for line in io.lines("lua/dickbutt.txt") do
+				irc_msg(params[1], line:gsub(" +$",""))
+			end
+			dickbutt_table[origin] = os.time()
+		else
+			irc_msg(params[1], origin.." is a dickbutt! (wait "..(os.time()-dickbutt_table[origin]+60).." seconds)")
 		end
 	end
 end
