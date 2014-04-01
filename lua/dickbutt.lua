2d2d57c699cfb14fdea22915ce655ea5ea2ffdbd
lq <lq@le1.ca>
2014-03-31 20:26:57 -0600

    dickbutt dickbutt dickbutt

---
+++ lua/dickbutt.lua
@@ -0,0 +1,9 @@
+function dickbutt_callback(event, origin, params)
+	local msg_parts = str_split_max(params[2], " ",2)
+	if msg_parts[1] == "!dickbutt" then
+		for line in io.lines("lua/dickbutt.txt") do
+			irc_msg(params[1], line)
+		end
+	end
+end
+register_callback("CHANNEL", "dickbutt_callback")
