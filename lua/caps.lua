a0e53d1f7ef1076988d07238e28315aafb47c44c
lq <lq@le1.ca>
2014-03-31 23:51:41 -0600

    bugfix

--- lua/caps.lua
+++ lua/caps.lua
@@ -6,16 +6,23 @@ function caps_callback(event, origin, params)
 		caps_table[origin] = 0
 	end
 	
-	-- increment or decrement entry
+	-- determine if its caps lock
+	local this_is_caps = false
 	if params[2]:upper() == params[2] and params[2]:lower() ~= params[2] then
+		this_is_caps = true
+	end
+	
+	-- increment or decrement entry
+	if this_is_caps then
 		caps_table[origin] = caps_table[origin] + 1
 	elseif caps_table[origin] > 0 then
 		caps_table[origin] = caps_table[origin] - 1
 	end
 	
-	if caps_table[origin] > 7 then
+	-- determine if we should kick
+	if caps_table[origin] > 7 and this_is_caps then
 		irc_raw("KICK "..params[1].." "..origin.." :i warned you!")
-	elseif caps_table[origin] > 5 then
+	elseif caps_table[origin] > 5 and this_is_caps then
 		irc_msg(params[1], origin..": calm down with the caps lock, bro!")
 	end
 	
