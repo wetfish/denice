bb7bfb23a41db1f7a3c42d7909bc0f05ff624915
lq <lq@le1.ca>
2014-04-01 20:56:59 +0400

    add custom character set to dickbutt

--- lua/dickbutt.lua
+++ lua/dickbutt.lua
@@ -3,9 +3,29 @@ dickbutt_table = {}
 function dickbutt_callback(event, origin, params)
 	local msg_parts = str_split_max(params[2], " ",2)
 	if msg_parts[1] == "!dickbutt" then
+		local rep = "dickbutt"
+		if msg_parts[2] ~= nil then
+			rep = msg_parts[2]:gsub(" +","")
+		end
+		print("using '"..rep.."' as string")
 		if dickbutt_table[origin] == nil or dickbutt_table[origin] < os.time() - 60 then
+			local rep_pos = 1
 			for line in io.lines("lua/dickbutt.txt") do
-				irc_msg(params[1], line:gsub(" +$",""))
+				local rline = ""
+				for i=1,line:len() do
+					print("line["..i.."] = "..line:sub(i,i))
+					print("rep["..rep_pos.."] = "..rep:sub(rep_pos,rep_pos))
+					if line:sub(i,i) == " " then
+						rline = rline .. " "
+					else
+						rline = rline .. rep:sub(rep_pos,rep_pos)
+						rep_pos = rep_pos + 1
+						if rep_pos > rep:len() then
+							rep_pos = 1
+						end
+					end
+				end
+				irc_msg(params[1], rline:gsub(" +$",""))
 			end
 			dickbutt_table[origin] = os.time()
 		else
