c27c8803d2cc8187d54b25fc478aa6f984cea568
lq <lq@le1.ca>
2014-04-02 00:51:06 -0600

    clean documentation

--- lua/help.lua
+++ lua/help.lua
@@ -4,7 +4,7 @@ function help_callback(event, origin, params)
 		for line in io.lines("lua/help.txt") do
 			-- ignore comments
 			if line:sub(1,1) ~= "#" then
-				irc_notice(origin, irc_color(line))
+				irc_msg(origin, irc_color(line))
 			end
 		end
 	end
