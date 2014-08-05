ceae4ead34d1a76a3e232af4232940bca21ebe33
lq <lq@le1.ca>
2014-08-06 00:02:50 +0400

    add roll command to roll dice

---
+++ lua/roll.lua
@@ -0,0 +1,49 @@
+function rolltrim(s, one)
+  local t = s:gsub("^%s*(.-)%s*$", "%1")
+  if t:len() < 1 then
+	t = one or 0
+  end
+  t = tonumber(t)
+  if t == nil then
+	t = 0
+  end
+  return math.floor(tonumber(t))
+end
+
+function roll_callback(event, origin, params)
+	local d_pos = params[2]:find("d")
+	local p_pos = params[2]:find("+") or params[2]:len()+1
+
+	if d_pos == nil then
+		return
+	end
+
+	local num = rolltrim(params[2]:sub(1,d_pos-1),1);
+	local size = rolltrim(params[2]:sub(d_pos+1, p_pos-1));
+	local plus = rolltrim(params[2]:sub(p_pos+1, params[2]:len())) or 0;
+
+	if num == nil or size == nil or num > 10 or num < 1 or size < 1 then
+		irc_msg(params[1], origin..": don't be a dick")
+		return
+	end
+
+	local str = origin.. " rolls: "
+	local sum = 0
+	for i=1,num do
+		local r = math.random(1, size)
+		str = str .. r
+		sum = sum + r
+		if i ~= num then
+			str = str .. " + "
+		end
+	end
+	if plus ~= 0 then
+		str = str .. " + "..plus
+		sum = sum + plus
+	end
+	if plus ~= 0 or num ~= 1 then
+		str = str .. " = " .. sum
+	end
+	irc_msg(params[1], str)
+end
+register_command("roll", "roll_callback")
