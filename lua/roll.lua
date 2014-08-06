1dfc0966c25fd9030e62ee8bc26a75ad695cb2c1
lq <lq@le1.ca>
2014-08-06 19:31:59 +0400

    support negative roll modifiers

--- lua/roll.lua
+++ lua/roll.lua
@@ -13,14 +13,20 @@ end
 function roll_callback(event, origin, params)
 	local d_pos = params[2]:find("d")
 	local p_pos = params[2]:find("+") or params[2]:len()+1
+	local m_pos = params[2]:find("-") or params[2]:len()+1
 
 	if d_pos == nil then
 		return
 	end
 
-	local num = rolltrim(params[2]:sub(1,d_pos-1),1);
-	local size = rolltrim(params[2]:sub(d_pos+1, p_pos-1));
-	local plus = rolltrim(params[2]:sub(p_pos+1, params[2]:len())) or 0;
+	local num = rolltrim(params[2]:sub(1,d_pos-1),1)
+	local size = rolltrim(params[2]:sub(d_pos+1, math.min(p_pos, m_pos) -1))
+	local plus = rolltrim(params[2]:sub(p_pos+1, params[2]:len())) or 0
+	local minus = rolltrim(params[2]:sub(m_pos+1, params[2]:len())) or 0
+
+	if minus ~= 0 then
+		plus = -1 * minus
+	end
 
 	if num == nil or size == nil or num > 10 or num < 1 or size < 1 then
 		irc_msg(params[1], origin..": don't be a dick")
