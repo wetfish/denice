26fdae9cfc58e32cdad38cbdcb618cac328f2fc6
lq <lq@le1.ca>
2014-08-10 19:41:28 +0400

    add example data for duel classes table

---
+++ duel_example.sql
@@ -0,0 +1,6 @@
+INSERT INTO `duelclasses` (`id`, `class`, `ac`, `attack`, `damage`, `hp`) VALUES
+(1, 'FIGHTER', 12, 4, 1, 20),
+(2, 'ROGUE', 15, 4, 3, 15),
+(3, 'DEFENDER', 13, 2, 0, 22),
+(4, 'BERSERKER', 12, 2, 3, 20);
+
