c87d244f6704e7fbc71eac82473bcb5527e3a419
lq <lq@le1.ca>
2014-08-10 19:37:50 +0400

    update sql structure

--- tables.sql
+++ tables.sql
@@ -1,13 +1,59 @@
-CREATE TABLE `dictionary` (
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+CREATE TABLE IF NOT EXISTS `dictionary` (
   `Index` int(11) NOT NULL AUTO_INCREMENT,
-  `Word1` varchar(32) NOT NULL,
-  `Word2` varchar(32) NOT NULL,
-  `Word3` varchar(32) NOT NULL,
+  `Word1` varchar(128) NOT NULL,
+  `Word2` varchar(128) NOT NULL,
+  `Word3` varchar(128) NOT NULL,
   `DateAdded` int(11) NOT NULL,
-  PRIMARY KEY (`Index`)
+  PRIMARY KEY (`Index`),
+  KEY `Word1` (`Word1`),
+  KEY `Word2` (`Word2`),
+  KEY `Word3` (`Word3`),
+  FULLTEXT KEY `Word1_2` (`Word1`,`Word2`,`Word3`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `quotes` (
+CREATE TABLE IF NOT EXISTS `duelchars` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nick` varchar(16) NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `armor` int(11) NOT NULL,
+  `attack` int(11) NOT NULL,
+  `damage` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `xp` int(11) NOT NULL,
+  `hp` int(11) NOT NULL,
+  `class` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `duelclasses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class` varchar(16) NOT NULL,
+  `ac` int(11) NOT NULL,
+  `attack` int(11) NOT NULL,
+  `damage` int(11) NOT NULL,
+  `hp` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `duelstats` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `player1` varchar(16) NOT NULL,
+  `player2` varchar(16) NOT NULL,
+  `p1wins` int(11) NOT NULL,
+  `p2wins` int(11) NOT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `greeting` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nick` varchar(32) NOT NULL,
+  `Greeting` text,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `quotes` (
   `Index` int(11) NOT NULL AUTO_INCREMENT,
   `Quote` varchar(512) NOT NULL,
   `Time` int(11) NOT NULL,
@@ -15,26 +61,19 @@ CREATE TABLE `quotes` (
   PRIMARY KEY (`Index`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `remember` (
+CREATE TABLE IF NOT EXISTS `remember` (
   `Index` int(11) NOT NULL AUTO_INCREMENT,
   `Title` text NOT NULL,
   `Content` text NOT NULL,
   PRIMARY KEY (`Index`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `seen` (
+CREATE TABLE IF NOT EXISTS `seen` (
   `nick` varchar(31) NOT NULL,
   `timestamp` int(11) NOT NULL,
   `event` varchar(16) NOT NULL,
-  `location` varchar(16) NOT NULL,
+  `location` text NOT NULL,
   `message` text NOT NULL,
   UNIQUE KEY `nick` (`nick`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `greeting` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Nick` varchar(32) NOT NULL,
-  `Greeting` text,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
