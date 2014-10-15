1d6f0740a35f9484b8f08736b953619a9b963281
lq <lq@le1.ca>
2014-10-15 23:44:04 +0400

    add blame in quotes

--- lua/quotes.lua
+++ lua/quotes.lua
@@ -8,12 +8,22 @@ function quote_cmd(event,origin,params) -- content,channel)
 	elseif params[2]:sub(0,1) == "#" then
 		irc_msg(params[1],quoteByIndex(params[2]:sub(2)))
 	elseif params[2]:sub(0,3) == "add" then
-		irc_msg(params[1],forceQuote(params[2]:sub(5)))
+		irc_msg(params[1],forceQuote(params[2]:sub(5),origin))
 	elseif params[2]:sub(0,3) == "del" and origin == get_config("bot:master") then
 		irc_msg(params[1],delQuote(params[2]:sub(5)))
+	elseif params[2]:sub(0,5) == "blame" then
+		irc_msg(params[1],quoteBlame(params[2]:sub(7)))
 	end
 end
 register_command("quote", "quote_cmd")
+
+function quoteBlame(index)
+	q = sql_query_fetch("SELECT `Blame` FROM `quotes` WHERE `Index`='"..sql_escape(index).."'")
+	if q[1]['Blame']:len() > 0 then
+		return "Blame " .. q[1]['Blame'] .. " for that one."
+	end
+	return "I don't know who to blame for that one."
+end
 
 function formatQuote(index,quote,time)
 	if quote == nil then
@@ -57,9 +67,9 @@ function quoteByIndex(qindex)
 	return ret or formatQuote()
 end
 
-function forceQuote(content)
+function forceQuote(content,user)
 	local time = os.time()
-	sql_fquery("INSERT INTO `quotes` (`Quote`,`Time`) VALUES ('"..sql_escape(content).."','"..sql_escape(time).."')")
+	sql_fquery("INSERT INTO `quotes` (`Quote`,`Time`,`Blame`) VALUES ('"..sql_escape(content).."','"..sql_escape(time).."','"..sql_escape(user).."')")
 	return "Quote added. (#"..sql_insert_id()..")"
 end
 
