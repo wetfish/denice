78edf4bde6c9692e23e77263c178426ba753b9d9
lq <lq@le1.ca>
2015-07-07 21:23:18 -0400

    add automatic nickserv identification

--- lua/connect.lua
+++ lua/connect.lua
@@ -1,6 +1,9 @@
 function connect_callback(event, origin, params)
 	local chanlist = get_config("bot:chans")
 	print("Now connected to "..origin)
+	if get_config("bot:nickservenable") ~= 0 then
+		irc_msg("NickServ", "identify " .. get_config("bot:nickservpass"))
+	end
 	for i,v in pairs(str_split(chanlist,",")) do
 		irc_join(v)
 	end
